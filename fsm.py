b16d28cc1d6a6bf22a7aa525b8d7beed18cfc5bd
yeh-sudo <yehchanshen@gmail.com>
2022-11-28 00:21:27 +0800

    fix state function bugs

--- fsm.py
+++ fsm.py
@@ -9,7 +9,7 @@ class TocMachine(GraphMachine):
 
     def is_going_to_searchCPU(self, event):
         text = event.message.text
-        return text.lower() == 'search cpu'
+        return text.lower() == "search cpu"
 
     def is_going_to_searchMotherboard(self, event):
         text = event.message.text
@@ -79,58 +79,67 @@ class TocMachine(GraphMachine):
         text = event.message.text
         return text.lower() == 'show psu'
 
-    def on_enter_searchCPU():
+    def on_enter_searchCPU(self, event):
         print('On search CPU')
+        self.go_back()
 
-    def on_enter_searchMotherboard():
+    def on_enter_searchMotherboard(self, event):
         print('On search Motherboard')
+        self.go_back()
     
-    def on_enter_searchRam():
+    def on_enter_searchRam(self, event):
         print('On search Ram')
+        self.go_back()
 
-    def on_enter_searchSSD():
+    def on_enter_searchSSD(self, event):
         print('On search SSD')
+        self.go_back()
 
-    def on_enter_searchAirCooler():
+    def on_enter_searchAirCooler(self, event):
         print('On search Air Cooler')
+        self.go_back()
 
-    def on_enter_searchAIO():
+    def on_enter_searchAIO(self, event):
         print('On search AIO')
+        self.go_back()
 
-    def on_enter_searchVGA():
+    def on_enter_searchVGA(self, event):
         print('On search VGA')
+        self.go_back()
 
-    def on_enter_searchCase():
+    def on_enter_searchCase(self, event):
         print('On search Case')
+        self.go_back()
 
-    def on_enter_searchPSU():
+    def on_enter_searchPSU(self, event):
         print('On search PSU')
+        self.go_back()
 
-    def on_enter_showCPU():
+    def on_enter_showCPU(self, event):
         print('On show CPU')
 
-    def on_enter_showMotherboard():
+    def on_enter_showMotherboard(self, event):
         print('On show Motherboard')
     
-    def on_enter_showRam():
+    def on_enter_showRam(self, event):
         print('On show Ram')
 
-    def on_enter_showSSD():
+    def on_enter_showSSD(self, event):
         print('On show SSD')
 
-    def on_enter_showAirCooler():
+    def on_enter_showAirCooler(self, event):
         print('On show Air Cooler')
 
-    def on_enter_showAIO():
+    def on_enter_showAIO(self, event):
         print('On show AIO')
 
-    def on_enter_showVGA():
+    def on_enter_showVGA(self, event):
         print('On show VGA')
 
-    def on_enter_showCase():
+    def on_enter_showCase(self, event):
         print('On show Case')
 
-    def on_enter_showPSU():
+    def on_enter_showPSU(self, event):
         print('On show PSU')
 
     
